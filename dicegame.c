f164828e31a79476f2b8e57cdb6f8c9006f0fe28
s1240009 <s1240009@std5dc35.u-aizu.ac.jp>
2018-07-02 16:46:25 +0900

    Initial revision

---
+++ dicegame.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+int main(){
+  int die1,die2,value;
+  srand((unsigned)time(NULL));
+  printf("Rolling the dice...\n");
+  die1=rand()%6+1;
+  die2=rand()%6+1;
+  printf("Die 1: %d\n",die1);
+  printf("Die 2: %d\n",die2);
+  value=die1+die2;
+  printf("Total value: %d\n",value);
+  return 0;
+}
